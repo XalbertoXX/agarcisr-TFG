347a0827608e914600e8f3f48dd83a9e318cba7a
AthrxL <a.garciasr.2018@alumnos.urjc.es>
2024-07-28 00:55:50 +0200

    Operation migration to rust library and Crystals Kyber addition

--- routes/app.py
+++ routes/app.py
@@ -1,223 +1,138 @@
 from flask import Flask, jsonify, request
-from cryptography.hazmat.primitives import hashes, serialization
-from cryptography.hazmat.primitives.asymmetric import ec, dh, rsa, padding
-from cryptography.hazmat.backends import default_backend
 import requests
-import numpy as np
+import hashlib
+import shadow_crypt  # Rust library
 
 app = Flask(__name__)
-
 
 # Diffie-Hellman key exchange
 @app.route('/diffie_hellman', methods=['GET'])
 def diffie_hellman_route():
-    # Generate Diffie-Hellman parameters and keys
-    parameters = dh.generate_parameters(generator=2, key_size=2048, backend=default_backend())
-    private_key = parameters.generate_private_key()
-    public_key = private_key.public_key()
-
-    # Serialize public key and send to Server 2
-    serialized_public_key = public_key.public_bytes(
-        encoding=serialization.Encoding.DER,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    response = requests.post('http://127.0.0.1:5001/receive_public_key', data=serialized_public_key)
+    # To be changed in next commits, just testing
+    p = "85859950916484676125439334608092681304789214056884991689423104627417791485869"  # Example prime
+    g = "2"   # Example generator
+    private_key, public_key = shadow_crypt.generate_dh_key(p, g)
+    response = requests.post('http://127.0.0.1:5001/receive_public_key', data=public_key)
 
     if response.status_code == 200:
         response_data = response.json()
         if 'server_public_key' in response_data:
-            try:
-                server_public_key = serialization.load_der_public_key(
-                    bytes.fromhex(response_data['server_public_key']), 
-                    backend=default_backend()
-                )
+            server_public_key = response_data['server_public_key']
+            shared_key = shadow_crypt.derive_dh_shared_key(private_key, server_public_key, p)
+            final_key = hashlib.sha256(shared_key.encode()).digest()
 
-                # Perform key exchange
-                shared_key = private_key.exchange(server_public_key)
-                derived_key = hashes.Hash(hashes.SHA256(), backend=default_backend())
-                derived_key.update(shared_key)
-                final_key = derived_key.finalize()
-
-                # Return private key and public keys
-                return jsonify({
-                    'success': True, 
-                    'server1_private_key': private_key.private_bytes(
-                        encoding=serialization.Encoding.PEM,
-                        format=serialization.PrivateFormat.PKCS8,
-                        encryption_algorithm=serialization.NoEncryption()
-                    ).hex(),
-                    'server1_public_key': serialized_public_key.hex(),
-                    'server2_public_key': response_data['server_public_key'],
-                    'final_key': final_key.hex()
-                })
-            except Exception as e:
-                return jsonify({'success': False, 'error': f'Error during key exchange: {str(e)}'})
+            return jsonify({
+                'success': True,
+                'server1_private_key': private_key,
+                'server1_public_key': public_key,
+                'server2_public_key': server_public_key,
+                'final_key': final_key.hex()
+            })
         else:
             return jsonify({'success': False, 'error': 'Server public key not found in response'})
     else:
         return jsonify({'success': False, 'error': f'Server 2 responded with status code {response.status_code}'})
-    
-    
-# Elliptic curve Diffie-Hellman key exchange
+
+# Elliptic Curve Diffie-Hellman key exchange
 @app.route('/ecdh', methods=['GET'])
 def ecdh_route():
-    try:
-        # Generate ECDH private and public keys
-        private_key = ec.generate_private_key(ec.SECP256R1(), default_backend())
-        public_key = private_key.public_key()
+    private_key, public_key = shadow_crypt.generate_ecdh_key()
+    response = requests.post('http://127.0.0.1:5001/receive_public_key_ell_curve', data=public_key)
 
-        # Serialize public key and send to Server 2
-        serialized_public_key = public_key.public_bytes(
-            encoding=serialization.Encoding.DER,
-            format=serialization.PublicFormat.SubjectPublicKeyInfo
-        )
-        response = requests.post('http://127.0.0.1:5001/receive_public_key_ell_curve', data=serialized_public_key)
+    if response.status_code == 200:
+        response_data = response.json()
+        if 'server_public_key' in response_data:
+            server_public_key = bytes.fromhex(response_data['server_public_key'])
+            shared_key = shadow_crypt.derive_ecdh_shared_key(private_key, server_public_key)
+            final_key = hashlib.sha256(shared_key).digest()
 
-        if response.status_code == 200:
-            response_data = response.json()
-            if 'server_public_key' in response_data:
-                server_public_key = serialization.load_der_public_key(
-                    bytes.fromhex(response_data['server_public_key']),
-                    backend=default_backend()
-                )
-
-                # Derive the shared key
-                shared_key = private_key.exchange(ec.ECDH(), server_public_key)
-                final_key = hashes.Hash(hashes.SHA256(), backend=default_backend())
-                final_key.update(shared_key)
-                final_key = final_key.finalize()
-
-                # Return private key and public keys
-                return jsonify({
-                    'success': True,
-                    'server1_private_key': private_key.private_bytes(
-                        encoding=serialization.Encoding.PEM,
-                        format=serialization.PrivateFormat.PKCS8,
-                        encryption_algorithm=serialization.NoEncryption()
-                    ).hex(),
-                    'server1_public_key': serialized_public_key.hex(),
-                    'server2_public_key': response_data['server_public_key'],
-                    'final_key': final_key.hex()
-                })
-            else:
-                return jsonify({'success': False, 'error': 'Server public key not found in response'})
+            return jsonify({
+                'success': True,
+                'server1_private_key': private_key.hex(),
+                'server1_public_key': public_key.hex(),
+                'server2_public_key': response_data['server_public_key'],
+                'final_key': final_key.hex()
+            })
         else:
-            return jsonify({'success': False, 'error': f'Server 2 responded with status code {response.status_code}'})
-    except Exception as e:
-        return jsonify({'success': False, 'error': f'Error during key exchange: {str(e)}'})
-
+            return jsonify({'success': False, 'error': 'Server public key not found in response'})
+    else:
+        return jsonify({'success': False, 'error': f'Server 2 responded with status code {response.status_code}'})
 
 # RSA encryption
 @app.route('/rsa', methods=['POST'])
 def rsa_route():
-
-    # Generate RSA keys inside the function
-    private_key_rsa = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048,
-        backend=default_backend()
-    )
-    public_key_rsa = private_key_rsa.public_key()
-    serialized_public_key_rsa = public_key_rsa.public_bytes(
-        encoding=serialization.Encoding.PEM,
-        format=serialization.PublicFormat.SubjectPublicKeyInfo
-    )
-    
+    private_key_rsa, public_key_rsa = shadow_crypt.generate_rsa_key()
     data = request.get_json()
-    message = data['message'].encode()
+    message = data['message']
 
-    # Send message and public key to protocols.py for encryption
     response = requests.post('http://127.0.0.1:5001/encrypt', json={
-        'message': message.hex(),
-        'public_key': serialized_public_key_rsa.hex()
+        'message': message,
+        'public_key': public_key_rsa
     })
 
     if response.status_code == 200:
         encrypted_message = response.json()['encrypted_message']
-        # Decrypt the message here for validation
-        decrypted_message = private_key_rsa.decrypt(
-            bytes.fromhex(encrypted_message),
-            padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(), label=None)
-        )
+        decrypted_message = shadow_crypt.rsa_decrypt(private_key_rsa, bytes.fromhex(encrypted_message))
         return jsonify({
             'success': True,
             'encrypted_message': encrypted_message,
-            'decrypted_message': decrypted_message.decode(),
-            'server1_public_key': serialized_public_key_rsa.hex(),
-            'server1_private_key': private_key_rsa.private_bytes(
-                encoding=serialization.Encoding.PEM,
-                format=serialization.PrivateFormat.PKCS8,
-                encryption_algorithm=serialization.NoEncryption()
-            ).hex()
+            'decrypted_message': decrypted_message,
+            'server1_public_key': public_key_rsa,
+            'server1_private_key': private_key_rsa
         })
     else:
         return jsonify({'success': False, 'error': 'Encryption failed'})
 
-
-# Swoosh functions (to be implemented differently in the final version)
-def swoosh_generate_parameters():
-    return {'q': 2**14, 'd': 256, 'N': 32}
-
-def swoosh_generate_keys(parameters):
-   q,  d, N = parameters['q'], parameters['d'], parameters['N']
-   A = np.random.randint(0, q, size=(N, N))
-   s = np.random.randint(-1, 2, size=N)
-   e = np.random.randint(-1, 2, size=N)
-   public_key = (np.dot(A, s) + e) % q
-   return s, public_key
-
-def swoosh_serialize_public_key(public_key):
-    return public_key.tobytes()
-
-def swoosh_deserialize_public_key(serialized_public_key):
-    return np.frombuffer(serialized_public_key, dtype=int)
-
-def swoosh_derive_shared_key(private_key, public_key):
-    q = 2**14
-    shared_key = (np.dot(private_key, public_key)) % q
-    return shared_key.tobytes()
-
-def swoosh_serialize_private_key(private_key):
-    return private_key.tobytes()
-
 # Swoosh NIKE key generation and exchange
 @app.route('/swoosh', methods=['GET'])
 def swoosh_nike_route():
-    try:
-        # Generate Swoosh parameters and keys
-        parameters = swoosh_generate_parameters()
-        private_key, public_key = swoosh_generate_keys(parameters)
+    parameters = (2**14, 256, 32)
+    private_key, public_key = shadow_crypt.swoosh_generate_keys(parameters)
+    serialized_public_key = public_key.hex()
+    response = requests.post('http://127.0.0.1:5001/swoosh_receive', data=serialized_public_key)
 
-        # Serialize public key and send to Server 2
-        serialized_public_key = swoosh_serialize_public_key(public_key)
-        response = requests.post('http://127.0.0.1:5001/swoosh_receive', data=serialized_public_key)
+    if response.status_code == 200:
+        response_data = response.json()
+        if 'server_public_key' in response_data:
+            server_public_key = bytes.fromhex(response_data['server_public_key'])
+            shared_key = shadow_crypt.swoosh_derive_shared_key(private_key, server_public_key, parameters[0])
+            final_key = hashlib.sha256(bytes(shared_key)).digest()
 
-        if response.status_code == 200:
-            response_data = response.json()
-            if 'server_public_key' in response_data:
-                server_public_key = swoosh_deserialize_public_key(bytes.fromhex(response_data['server_public_key']))
-
-                # Derive the shared key
-                shared_key = swoosh_derive_shared_key(private_key, server_public_key)
-                final_key = hashes.Hash(hashes.SHA256(), backend=default_backend())
-                final_key.update(shared_key)
-                final_key = final_key.finalize()
-
-                # Return keys and shared secret
-                return jsonify({
-                    'success': True,
-                    'server1_private_key': swoosh_serialize_private_key(private_key).hex(),
-                    'server1_public_key': serialized_public_key.hex(),
-                    'server2_public_key': response_data['server_public_key'],
-                    'final_key': final_key.hex()
-                })
-            else:
-                return jsonify({'success': False, 'error': 'Server public key not found in response'})
+            return jsonify({
+                'success': True,
+                'server1_private_key': private_key.hex(),
+                'server1_public_key': serialized_public_key,
+                'server2_public_key': response_data['server_public_key'],
+                'final_key': final_key.hex()
+            })
         else:
-            return jsonify({'success': False, 'error': f'Server 2 responded with status code {response.status_code}'})
-    except Exception as e:
-        return jsonify({'success': False, 'error': f'Error during key exchange: {str(e)}'})
+            return jsonify({'success': False, 'error': 'Server public key not found in response'})
+    else:
+        return jsonify({'success': False, 'error': f'Server 2 responded with status code {response.status_code}'})
 
+# Crystals Kyber key encapsulation
+@app.route('/kyber', methods=['GET'])
+def kyber_route():
+    public_key, secret_key = shadow_crypt.kyber_keygen()
+    response = requests.post('http://127.0.0.1:5001/kyber_encapsulate', data=public_key)
+
+    if response.status_code == 200:
+        response_data = response.json()
+        if 'ciphertext' in response_data:
+            ciphertext = bytes.fromhex(response_data['ciphertext'])
+            shared_secret = shadow_crypt.kyber_decapsulate(ciphertext, secret_key)
+
+            return jsonify({
+                'success': True,
+                'server1_public_key': public_key.hex(),
+                'server1_secret_key': secret_key.hex(),
+                'server2_ciphertext': response_data['ciphertext'],
+                'shared_secret': shared_secret.hex()
+            })
+        else:
+            return jsonify({'success': False, 'error': 'Ciphertext not found in response'})
+    else:
+        return jsonify({'success': False, 'error': f'Server 2 responded with status code {response.status_code}'})
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
